e79e2369a9cb4e8b687ec3c85ceca4064c76ac62
UselessAmber <amberthequeer@gmail.com>
2022-12-03 17:38:41 +1100

    Day 3 finished

--- Day-3-pt2.cpp
+++ Day-3-pt2.cpp
@@ -6,12 +6,13 @@ using namespace std;
 const int N = 1e3 + 7;
 ifstream fi("input.txt");
 ofstream fo("output.txt");
-char bag[2][N];
+char bag[3][N];
 
 void clear(){
     for (int i = 0; i < N; i++){
         bag[0][i] = 0;
         bag[1][i] = 0;
+        bag[2][i] = 0;
     }
 }
 
@@ -25,9 +26,11 @@ int convert(char c){
 
 int search(int n){
     int i = 0;
-    for (int j = 0; j < n; j++){
-        while (bag[0][i] < bag[1][j]) i++;
-        if (bag[0][i] == bag[1][j]){
+    int j = 0;
+    for (int k = 0; k < n; k++){
+        while (bag[0][i] < bag[2][k]) i++;
+        while (bag[1][j] < bag[2][k]) j++;
+        if (bag[0][i] == bag[1][j] && bag[1][j] == bag[2][k]){
             return convert(bag[0][i]);
         }
     }
@@ -39,15 +42,21 @@ int main(){
     int ans = 0;
     while (fi >> inp){
         clear();
-        for (int i = 0; i < len(inp) / 2; i++){
+        for (int i = 0; i < len(inp); i++){
             bag[0][i] = inp[i];
         }
-        for (int i = len(inp) / 2; i < len(inp); i++){
-            bag[1][i - len(inp) / 2] = inp[i];
+        sort(bag[0], bag[0] + len(inp));
+        fi >> inp;
+        for (int i = 0; i < len(inp); i++){
+            bag[1][i] = inp[i];
         }
-        sort(bag[0], bag[0] + len(inp) / 2);
-        sort(bag[1], bag[1] + len(inp) / 2);
-        ans += search(len(inp) / 2);
+        sort(bag[1], bag[1] + len(inp));
+        fi >> inp;
+        for (int i = 0; i < len(inp); i++){
+            bag[2][i] = inp[i];
+        }
+        sort(bag[2], bag[2] + len(inp));
+        ans += search(len(inp));
     }
     fo << ans;
 }
